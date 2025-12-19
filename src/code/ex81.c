eb22670beec12f4785508eaadb811d1efc45584c
sankaku789 <sankaku789.1000@gmail.com>
2025-12-19 14:21:42 +0900

    タスクの説明

---
+++ src/code/ex81.c
@@ -0,0 +1,138 @@
+/*
+ *  EX (Exercise for RTOS)
+ * 
+ *  Copyright (C) 2001-2019 by Dep. of Computer Science and Engineering
+ *                   Tomakomai National College of Technology, JAPAN
+ *
+ *  ��L���쌠�҂́C�ȉ��� (1)�`(4) �̏������CFree Software Foundation 
+ *  �ɂ���Č��\����Ă��� GNU General Public License �� Version 2 �ɋL
+ *  �q����Ă�������𖞂����ꍇ�Ɍ���C�{�\�t�g�E�F�A�i�{�\�t�g�E�F�A
+ *  �����ς������̂��܂ށD�ȉ������j���g�p�E�����E���ρE�Ĕz�z�i�ȉ��C
+ *  ���p�ƌĂԁj���邱�Ƃ𖳏��ŋ�������D
+ *  (1) �{�\�t�g�E�F�A���\�[�X�R�[�h�̌`�ŗ��p����ꍇ�ɂ́C��L�̒���
+ *      ���\���C���̗��p��������щ��L�̖��ۏ؋K�肪�C���̂܂܂̌`�Ń\�[
+ *      �X�R�[�h���Ɋ܂܂�Ă��邱�ƁD
+ *  (2) �{�\�t�g�E�F�A���C���C�u�����`���ȂǁC���̃\�t�g�E�F�A�J���Ɏg
+ *      �p�ł���`�ōĔz�z����ꍇ�ɂ́C�Ĕz�z�ɔ����h�L�������g�i���p
+ *      �҃}�j���A���Ȃǁj�ɁC��L�̒��쌠�\���C���̗��p��������щ��L
+ *      �̖��ۏ؋K����f�ڂ��邱�ƁD
+ *  (3) �{�\�t�g�E�F�A���C�@��ɑg�ݍ��ނȂǁC���̃\�t�g�E�F�A�J���Ɏg
+ *      �p�ł��Ȃ��`�ōĔz�z����ꍇ�ɂ́C���̏����𖞂������ƁD
+ *    (a) �Ĕz�z�ɔ����h�L�������g�i���p�҃}�j���A���Ȃǁj�ɁC��L�̒�
+ *        �쌠�\���C���̗��p��������щ��L�̖��ۏ؋K����f�ڂ��邱�ƁD
+ *  (4) �{�\�t�g�E�F�A�̗��p�ɂ�蒼�ړI�܂��͊ԐړI�ɐ����邢���Ȃ鑹
+ *      �Q������C��L���쌠�҂����TOPPERS�v���W�F�N�g��Ɛӂ��邱�ƁD
+ *
+ *  �{�\�t�g�E�F�A�́C���ۏ؂Œ񋟂���Ă�����̂ł���D��L���쌠�҂�
+ *  ���TOPPERS�v���W�F�N�g�́C�{�\�t�g�E�F�A�Ɋւ��āC���̓K�p�\����
+ *  �܂߂āC�����Ȃ�ۏ؂��s��Ȃ��D�܂��C�{�\�t�g�E�F�A�̗��p�ɂ�蒼
+ *  �ړI�܂��͊ԐړI�ɐ����������Ȃ鑹�Q�Ɋւ��Ă��C���̐ӔC�𕉂�Ȃ��D
+ * 
+ *  @(#) $Id: $
+ */
+
+/*
+ *  RTOS ���K
+ *
+ *  ex80.c -- �C�x���g�t���O�ɂ��^�X�N�ԓ����v���O����
+ */
+
+#include "ex.h"
+
+void task1 (intptr_t exinf)
+{
+	int	count = 0;
+	char ch;
+	FLGPTN	flag = 0x00;
+	SYSTIM	time;
+
+	taskinfo(exinf);
+	get_tim(&time);
+	syslog(LOG_NOTICE, "[  :%2d.%03d] TASKID:%2d, Count:%2d",
+	       time / 1000, time % 1000, exinf, count);
+	while (1) {
+		tslp_tsk(1000);
+		count ++;
+		serial_rea_dat(CONSOLE_PORTID, &ch, sizeof(ch));
+		get_tim(&time);
+		
+		switch(ch) {
+			case 'r':
+			case 'R':
+				flag ^= 0x01;
+				break;
+			case 'g':
+			case 'G':
+				flag ^= 0x08;
+				break;
+			case 'b':
+			case 'B':
+				flag ^= 0x04;
+				break;
+			case 'y':
+			case 'Y':
+				flag ^= 0x02;
+				break;
+			case 'a':
+			case 'A':
+				flag = 0x0f;
+				break;
+			case 'c':
+			case 'C':
+				flag = 0x00;
+				break;
+			case 'l':
+			case 'L':
+				flag ^= 0x03;
+				break;
+			case 'u':
+			case 'U':
+				flag ^= 0x0c;
+				break;
+		}
+			
+		if (flag == 0x00) {
+			flag |= 0x10;
+		}
+
+		syslog(LOG_NOTICE, "[  :%2d.%03d] TASKID:%2d, CH: 0x%02x, FLG: 0x%02x",
+			    time / 1000, time % 1000, exinf, ch, flag);
+		set_flg(FLG1, flag);
+		
+	}
+}
+
+void task2 (intptr_t exinf)
+{
+	int	count = 0;
+	FLGPTN	flag = 0x00;
+	SYSTIM	time;
+
+	taskinfo(exinf);
+	get_tim(&time);
+	syslog(LOG_NOTICE, "[  :%2d.%03d] TASKID:%2d, Count:%2d",
+	       time / 1000, time % 1000, exinf, count);
+	while (1) {
+		wai_flg(FLG1, 0x1f, TWF_ORW, &flag);
+		clr_flg(FLG1, 0x00);
+
+		count ++;
+		get_tim(&time);
+		syslog(LOG_NOTICE, "[  :%2d.%03d] TASKID:%2d, Count:%2d, flag: 0x%02x",
+		       time / 1000, time % 1000, exinf, count, flag & 0x0f);
+		sil_wrb_mem((void *)PORTD_DR_ADDR, flag & 0x0f);
+	}
+}
+
+void main_task (intptr_t exinf)
+{
+	/* 
+	 * flag�A�|�[�gD �̐ݒ�: �o��
+	 */
+	sil_wrb_mem((void *)PORTD_DDR_ADDR, 0x0f);
+
+	taskinfo(exinf);
+	act_tsk(TASK1);
+	act_tsk(TASK2);
+	slp_tsk();
+}
